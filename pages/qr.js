0f38dcb8bfc36390600615ce245928b6f8407035
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-08-25 15:00:22 +1000

    .

--- pages/qr.js
+++ pages/qr.js
@@ -1,7 +1,19 @@
 export default function Qr() {
   return (
-    <div>
-      <img src="/qr-tberghuis-dev.svg" alt="" />
+    <div className="flex flex-col justify-center min-h-screen">
+      <div className="text-center basis-1/4">
+        <a className="btn" href="https://tberghuis.dev">
+          tberghuis.dev
+        </a>
+      </div>
+      {/* https://qrd.by/qr-code-generator-svg */}
+      <div className="basis-3/4">
+        <img
+          className="mx-auto"
+          src="/qr-tberghuis-dev.svg"
+          alt="tberghuis.dev QR code"
+        />
+      </div>
     </div>
   );
 }
